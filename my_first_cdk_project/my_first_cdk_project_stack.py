ac06154231c4dc52dfe3933baa76256236de0cef
VishalKambli-1 <vishal03k@gmail.com>
2022-08-29 13:33:08 +0530

    First commit after staging

--- my_first_cdk_project/my_first_cdk_project_stack.py
+++ my_first_cdk_project/my_first_cdk_project_stack.py
@@ -1,18 +1,21 @@
 from aws_cdk import (
-    # Duration,
-    Stack,
+    aws_s3 as _s3,
+    core
     # aws_sqs as sqs,
 )
 
-class MyFirstCdkProjectStack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+class MyFirstCdkProjectStack(core.Stack):
+
+    def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "MyFirstCdkProjectQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        _s3.Bucket(
+            self,
+            "myBucketId",
+            bucket_name="myfirstcdkproject7534",
+            versioned=True,
+            encryption=_s3.BucketEncryption.KMS_MANAGED
+        )
